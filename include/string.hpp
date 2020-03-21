2c7aaab52de45d854acec2bdb876a7bdc11d6d59
Skvortsovvv <56050761+Skvortsovvv@users.noreply.github.com>
2020-03-21 10:45:55 +0300

    Update string.hpp

--- include/string.hpp
+++ include/string.hpp
@@ -8,30 +8,30 @@
 
 class String
 {
-	char* ptr;
+    char* ptr;
 public:
-	~String();
-	String();
-	String(const String& str);
-	String(const char* data);
-	String& operator=(const String& str);
-	String& operator+=(const String& str);
-	String& operator*=(unsigned int m);
-	bool operator==(String& str);
-	size_t Find(const String& str) const;
-	void Replace(char oldSymbol, char newSymbol);
-	size_t Size() const;
-	bool Empty();
-	char operator[](size_t index) const;
-	char& operator[](size_t index);
-	void RTrim(char symbol);
-	void LTrim(char symbol);
-	void swap(String& oth);
-	friend std::ostream& operator<<(std::ostream&, const String&);
-	friend String operator+(const String& a, const String& b);
-	friend String operator*(const String& a, unsigned int b);
-	friend bool operator!=(const String& a, const String& b);
-	friend bool operator>(const String& a, const String& b);
+    ~String();
+    String();
+    String(const String& str);
+    String(const char* data);
+    String& operator=(const String& str);
+    String& operator+=(const String& str);
+    String& operator*=(unsigned int m);
+    bool operator==(String& str);
+    size_t Find(const String& str) const;
+    void Replace(char oldSymbol, char newSymbol);
+    size_t Size() const;
+    bool Empty();
+    char operator[](size_t index) const;
+    char& operator[](size_t index);
+    void RTrim(char symbol);
+    void LTrim(char symbol);
+    void swap(String& oth);
+    friend std::ostream& operator<<(std::ostream&, const String&);
+    friend String operator+(const String& a, const String& b);
+    friend String operator*(const String& a, unsigned int b);
+    friend bool operator!=(const String& a, const String& b);
+    friend bool operator>(const String& a, const String& b);
 };
 
 #endif  // INCLUDE_STRING_HPP_
