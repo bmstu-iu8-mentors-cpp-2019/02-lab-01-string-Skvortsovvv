b3062217646c69e92de8bd4cd7dc41bda86af5ee
Skvortsovvv <56050761+Skvortsovvv@users.noreply.github.com>
2020-03-21 10:27:53 +0300

    Update source.cpp

--- source/source.cpp
+++ source/source.cpp
@@ -11,111 +11,111 @@ String operator+(const String& a, const String& b) {
     ps[a.Size() + b.Size()] = '\0';
     for (size_t i = 0; i < a.Size(); i++) {
         ps[i] = a.ptr[i];
-	}
-	for (size_t i = a.Size(), j = 0; i < a.Size() + b.Size(); i++, j++) {
-		ps[i] = b.ptr[j];
-	}
-	String* str = new String(ps);
-	delete[]ps;
-	return *str;
+    }
+    for (size_t i = a.Size(), j = 0; i < a.Size() + b.Size(); i++, j++) {
+        ps[i] = b.ptr[j];
+    }
+    String* str = new String(ps);
+    delete[]ps;
+    return *str;
 }
 
 String operator*(const String& a, unsigned int b) {
-	char* ps = new char[a.Size() * b + 1];
-	ps[a.Size() * b] = '\0';
-	for (size_t i = 0, j = 0; i < (a.Size() * b); i++, j++) {
-		if (j == a.Size()) j = 0;
-		ps[i] = a.ptr[j];
-	}
-	String* str = new String(ps);
-	delete[]ps;
-	return *str;
+    char* ps = new char[a.Size() * b + 1];
+    ps[a.Size() * b] = '\0';
+    for (size_t i = 0, j = 0; i < (a.Size() * b); i++, j++) {
+        if (j == a.Size()) j = 0;
+        ps[i] = a.ptr[j];
+    }
+    String* str = new String(ps);
+    delete[]ps;
+    return *str;
 }
 
 bool operator!=(const String& a, const String& b) {
-	size_t count = 0;
-	if (a.Size() != b.Size()) return true;
-	else {
-		for (size_t i = 0; i < a.Size(); i++) {
-			if (a.ptr[i] == b.ptr[i]) count++;
-		}
-		if (count == a.Size()) return false;
-		else return true;
-	}
+    size_t count = 0;
+    if (a.Size() != b.Size()) return true;
+    else {
+        for (size_t i = 0; i < a.Size(); i++) {
+            if (a.ptr[i] == b.ptr[i]) count++;
+        }
+        if (count == a.Size()) return false;
+        else return true;
+    }
 }
 
 bool operator>(const String& a, const String& b) {
-	if (a.Size() > b.Size()) return true;
-	else return false;
+    if (a.Size() > b.Size()) return true;
+    else return false;
 }
 
 String::~String() {
-	delete[]ptr;;
+    delete[]ptr;;
 }
 String::String() {
-	ptr = nullptr;
+    ptr = nullptr;
 }
 String::String(const String& str) {
-	size_t len = str.Size();
-	ptr = new char[len + 1];
-	ptr[len] = '\0';
-	delete[]ptr;
-	for (size_t i = 0; i < len; i++) {
-		ptr[i] = str.ptr[i];
-	}
+    size_t len = str.Size();
+    ptr = new char[len + 1];
+    ptr[len] = '\0';
+    delete[]ptr;
+    for (size_t i = 0; i < len; i++) {
+        ptr[i] = str.ptr[i];
+    }
 }
 String::String(const char* data) {
-	ptr = new char[strlen(data) + 1];
-	ptr[strlen(data)] = '\0';
-	for (size_t i = 0; ptr[i] != '\0'; i++) {
-		ptr[i] = data[i];
-	}
+    ptr = new char[strlen(data) + 1];
+    ptr[strlen(data)] = '\0';
+    for (size_t i = 0; ptr[i] != '\0'; i++) {
+        ptr[i] = data[i];
+    }
 }
 String& String::operator=(const String& str) {
-	size_t len = str.Size();
-	delete[]ptr;
-	char* ps = new char[len + 1];
-	ps[len] = '\0';
-	for (size_t i = 0; i < len; i++) {
-		ps[i] = str.ptr[i];
-	}
-	ptr = ps;
-	return *this;
+    size_t len = str.Size();
+    delete[]ptr;
+    char* ps = new char[len + 1];
+    ps[len] = '\0';
+    for (size_t i = 0; i < len; i++) {
+        ps[i] = str.ptr[i];
+    }
+    ptr = ps;
+    return *this;
 }
 String& String::operator+=(const String& str) {
-	char* newstr = new char[this->Size() + str.Size() + 1];
-	newstr[this->Size() + str.Size()] = '\0';
-	for (size_t i = 0; i < this->Size(); i++) {
-		newstr[i] = ptr[i];
-	}
-	for (size_t i = this->Size(), j = 0; i < this->Size() + str.Size(); i++, j++) {
-		newstr[i] = str.ptr[j];
-	}
-	delete[]ptr;
-	ptr = newstr;
-	return *this;
+    char* newstr = new char[this->Size() + str.Size() + 1];
+    newstr[this->Size() + str.Size()] = '\0';
+    for (size_t i = 0; i < this->Size(); i++) {
+        newstr[i] = ptr[i];
+    }
+    for (size_t i = this->Size(), j = 0; i < this->Size() + str.Size(); i++, j++) {
+        newstr[i] = str.ptr[j];
+    }
+    delete[]ptr;
+    ptr = newstr;
+    return *this;
 }
 String& String::operator*=(unsigned int m) {
-	char* ps = new char[this->Size() * m + 1];
-	ps[this->Size() * m] = '\0';
-	for (size_t i = 0, j = 0; i < this->Size() * m; i++, j++) {
-		if (j == this->Size()) j = 0;
-		ps[i] = ptr[j];
-	}
-	delete[]ptr;
-	ptr = ps;
-	return *this;
+    char* ps = new char[this->Size() * m + 1];
+    ps[this->Size() * m] = '\0';
+    for (size_t i = 0, j = 0; i < this->Size() * m; i++, j++) {
+        if (j == this->Size()) j = 0;
+        ps[i] = ptr[j];
+    }
+    delete[]ptr;
+    ptr = ps;
+    return *this;
 }
 bool String::operator==(String& str) {
-	int count = 0;
-	if (this->Size() == str.Size()) {
-		for (size_t i = 0; i < this->Size(); i++) {
-			if (ptr[i] == str.ptr[i]) count++;
-		}
-		if (count == this->Size()) return true;
-		else return false;
-	}
-	else return false;
+    int count = 0;
+    if (this->Size() == str.Size()) {
+        for (size_t i = 0; i < this->Size(); i++) {
+            if (ptr[i] == str.ptr[i]) count++;
+        }
+        if (count == this->Size()) return true;
+        else return false;
+    }
+    else return false;
 }
 size_t String::Find(const String& str) const {
 	size_t count = 0, key = -1;
