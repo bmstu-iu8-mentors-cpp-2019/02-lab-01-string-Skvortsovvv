3512b908f8b6456ad5e4fbaec49738878bf9468b
Skvortsovvv <56050761+Skvortsovvv@users.noreply.github.com>
2020-03-24 11:31:43 +0300

    Update source.cpp

--- source/source.cpp
+++ source/source.cpp
@@ -233,15 +233,15 @@ void String::Swap(String& oth) {
 }
 
 bool String::operator==(const char* str) const {
-	if (this->Size() == strlen(str)) {
-		size_t count = 0;
-		for (size_t i = 0; i < this->Size(); i++) {
-			if (ptr[i] == str[i]) {
-				count++;
-			}
-		}
-		if (count == this->Size()) return true;
-	} else {
-		return false;
-	}
+    if (this->Size() == strlen(str)) {
+        size_t count = 0;
+        for (size_t i = 0; i < this->Size(); i++) {
+            if (ptr[i] == str[i]) {
+                count++;
+            }
+        }
+        if (count == this->Size()) return true;
+    } else {
+        return false;
+    }
 }
